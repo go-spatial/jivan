cf87be5cb090ae86a66936295a3e05a8327806aa
Jivan Amara <Development@JivanAmara.net>
2018-03-22 18:04:50 -0600

    Clean up handlers.

--- server/handlers.go
+++ server/handlers.go
@@ -31,21 +31,13 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"regexp"
-	"sort"
 	"strconv"
-	"strings"
 
-	"github.com/go-spatial/go-wfs/data_provider"
 	"github.com/go-spatial/tegola/geom/encoding/geojson"
 	"github.com/julienschmidt/httprouter"
 )
 
-// Values returned by content providers (such as root(), conformance()) need to implement this interface
-type ContentUpdater interface {
-	// Updates any content type references in the struct and contained structs w/ contentType
-	ContentType(contentType string)
-}
+const DEFAULT_PAGE_SIZE = 10
 
 // Sets response 'status', and writes a json-encoded object with property "detail" having value "msg".
 func jsonError(w http.ResponseWriter, msg string, status int) {
@@ -61,25 +53,6 @@ func jsonError(w http.ResponseWriter, msg string, status int) {
 		w.Write([]byte(fmt.Sprintf("problem marshaling error: %v", msg)))
 	} else {
 		w.Write(result)
-	}
-}
-
-// -- Writes a 404 response
-func notFoundError(w http.ResponseWriter) {
-	jsonError(w, "not found", 404)
-}
-
-func conformanceJson(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	c := conformance()
-	result, err := json.Marshal(c)
-	w.Header().Set("Content-Type", "application/json")
-
-	if err != nil {
-		jsonError(w, "problem marshaling conformance declaration to json", 500)
-		return
-	} else {
-		w.WriteHeader(200)
-		w.Write([]byte(result))
 	}
 }
 
@@ -99,6 +72,20 @@ func rootJson(w http.ResponseWriter, r *http.Request, params httprouter.Params) 
 	w.Write(rJson)
 }
 
+func conformanceJson(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	c := conformance()
+	result, err := json.Marshal(c)
+	w.Header().Set("Content-Type", "application/json")
+
+	if err != nil {
+		jsonError(w, "problem marshaling conformance declaration to json", 500)
+		return
+	} else {
+		w.WriteHeader(200)
+		w.Write([]byte(result))
+	}
+}
+
 // --- Return the json-encoded OpenAPI 3 spec for the WFS API available on this instance.
 func openapiJson(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	status := 200
@@ -106,100 +93,6 @@ func openapiJson(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	w.WriteHeader(status)
 	w.Write(OpenAPI3SchemaJSON)
 }
-
-// --- Return the names of feature layers available in current provider
-func collectionNames(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("content-type", "application/json")
-
-	ftNames, err := Provider.CollectionNames()
-	if err != nil {
-		jsonError(w, err.Error(), 500)
-		return
-	}
-
-	layersJSON, err := json.Marshal(ftNames)
-	if err != nil {
-		jsonError(w, err.Error(), 500)
-	}
-
-	w.Write(layersJSON)
-}
-
-// --- Return the ids of features available in the named collection (layer) for current provider
-func featurePks(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("content-type", "application/json")
-	reqQuery := r.URL.Query()
-	var collectionNames []string = reqQuery["collection"]
-
-	if len(collectionNames) < 1 {
-		// No collection specified to filter on indicates all collections
-		var err error
-		collectionNames, err = Provider.CollectionNames()
-		if err != nil {
-			jsonError(w, err.Error(), 500)
-		}
-	}
-	sort.Strings(collectionNames)
-
-	fids := make([]data_provider.FeatureId, 0, 100)
-	for _, cn := range collectionNames {
-		fs, err := Provider.CollectionFeatures(cn, nil)
-		if err != nil {
-			jsonError(w, err.Error(), 500)
-		}
-		for _, f := range fs {
-			fids = append(fids, data_provider.FeatureId{Collection: cn, FeaturePk: f.ID})
-		}
-	}
-
-	fmt.Printf("getFeatureIds len: %v\n", len(fids))
-	idsJSON, err := json.Marshal(fids)
-	if err != nil {
-		w.WriteHeader(500)
-		w.Write([]byte(fmt.Sprintf(`{ "detail": "%v"`, err)))
-		return
-	}
-
-	w.Write(idsJSON)
-}
-
-// --- Return all data, esp. geometry for requested feature
-func getFeature(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("content-type", "application/json")
-
-	reqQuery := r.URL.Query()
-	idStr := reqQuery.Get("id")
-
-	split := strings.Split(idStr, "-")
-	featurePkStr := split[len(split)-1]
-	// strip off the feature id portion of the string plus the "-" separator
-	collectionName := idStr[:len(idStr)-(len(featurePkStr)+1)]
-
-	featurePk, err := strconv.ParseUint(featurePkStr, 10, 64)
-
-	featureId := data_provider.FeatureId{Collection: collectionName, FeaturePk: featurePk}
-
-	featureData, err := Provider.GetFeatures([]data_provider.FeatureId{featureId})
-	if err != nil {
-		w.WriteHeader(500)
-		w.Write([]byte(fmt.Sprintf(`{ "detail": "%v" }`, err)))
-		return
-	}
-	f := featureData[0]
-
-	gf := geojson.Feature{ID: &f.ID, Geometry: geojson.Geometry{Geometry: f.Geometry}}
-	encoding, err := json.Marshal(gf)
-	if err != nil {
-		jsonError(w, err.Error(), 500)
-		return
-	}
-
-	w.Write(encoding)
-}
-
-var collectionPathRegexp = regexp.MustCompile(`^/collection/(\w+)$`)
-
-const DEFAULT_PAGE_SIZE = 10
 
 func collectionMetaDataJson(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
 	ct := "application/json"
