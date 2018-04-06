8235c9e5b47ac9d49424f289f070cfb32ecaee45
James Lucktaylor <jlucktay@gmail.com>
2018-04-06 17:03:37 +0100

    Finished test coverage, and edited function name

--- defaultgpkg/main_test.go
+++ defaultgpkg/main_test.go
@@ -27,7 +27,6 @@
 package defaultgpkg
 
 import (
-	"log"
 	"os"
 	"path"
 	"runtime"
@@ -38,41 +37,46 @@ var baseTestPath string
 
 func init() {
 	_, thisFilePath, _, _ := runtime.Caller(0)
-	baseTestPath = path.Join(path.Dir(thisFilePath), "../test_data/main.defaultGpkg/")
+	baseTestPath = path.Join(path.Dir(thisFilePath), "testdata")
 }
 
 func TestDefaultGpkg(t *testing.T) {
-	// For use when a data source isn't provided.
-	// Scans for files w/ '.gpkg' extension and chooses the first alphabetically.
-	// * First scan the working directory
-	// * Second, if none found in the working directory and it exists, scan ./data/
-	// * Third, if none found in either of the previous and it exists, scan ./test_data/
-	// Returns a full path to the file or an empty string if none found.
-
 	cases := []struct {
-		// The name of the sub-directory, under ./test_data/main.defaultGpkg/
-		in string
+		// The name of the sub-directory, under ./testdata/
+		testSubPath string
 
 		// The full path to the selected file, or an empty string if no file is found
-		want string
+		dummyFileName string
 	}{
 		{
-			in:   "1",
-			want: "",
+			testSubPath:   "1",
+			dummyFileName: path.Join(baseTestPath, "1/dummyA.gpkg"),
+		},
+		{
+			testSubPath:   "2",
+			dummyFileName: path.Join(baseTestPath, "2/data/dataDummy.gpkg"),
+		},
+		{
+			testSubPath:   "3",
+			dummyFileName: path.Join(baseTestPath, "3/test_data/testDataDummy.gpkg"),
+		},
+		{
+			testSubPath:   "4",
+			dummyFileName: "",
 		},
 	}
 
 	for _, c := range cases {
-		casePath := path.Join(baseTestPath, c.in)
+		casePath := path.Join(baseTestPath, c.testSubPath)
 
 		if chdirErr := os.Chdir(casePath); chdirErr != nil {
-			log.Fatalf("Could not change working directory to '%s'.", casePath)
+			t.Errorf("Could not change working directory to '%s'.", casePath)
 		}
 
-		got := DefaultGpkg()
+		got := Get()
 
-		if got != c.want {
-			t.Errorf("Under '%s', defaultGpkg() == '%s', wanted '%s'.", casePath, got, c.want)
+		if got != c.dummyFileName {
+			t.Errorf("Under '%s', defaultGpkg() == '%s', wanted '%s'.", casePath, got, c.dummyFileName)
 		}
 	}
 }
