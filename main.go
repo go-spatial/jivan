328e438d2d23a75be9cd463596ea4738ed773266
James Lucktaylor <jlucktay@gmail.com>
2018-04-11 09:22:26 +0100

    Merging...

--- main.go
+++ main.go
@@ -30,52 +30,15 @@ package main
 import (
 	"flag"
 	"fmt"
-	"log"
 	"os"
-	"path"
-	"path/filepath"
-	"sort"
 
 	"github.com/go-spatial/tegola/provider/gpkg"
 	"github.com/jlucktay/go-wfs/config"
 	"github.com/jlucktay/go-wfs/data_provider"
+	"github.com/jlucktay/go-wfs/defaultgpkg"
 	"github.com/jlucktay/go-wfs/server"
 	"github.com/jlucktay/go-wfs/wfs3"
 )
-
-// For use when a data source isn't provided.
-// Scans for files w/ '.gpkg' extension and chooses the first alphabetically.
-// * First scan the working directory
-// * Second, if none found in the working directory and it exists, scan ./data/
-// * Third, if none found in either of the previous and it exists, scan ./test_data/
-// Returns a full path to the file or an empty string if none found.
-func defaultGpkg() string {
-	// Directories to check in decreasing order of priority
-	dirs := []string{"", "data", "test_data"}
-	gpkgPath := ""
-	wd, err := os.Getwd()
-	if err != nil {
-		log.Printf("Unable to get working directory: %v", err)
-		return ""
-	}
-
-	for _, dir := range dirs {
-		searchGlob := path.Join(dir, "*.gpkg")
-		gpkgFiles, err := filepath.Glob(searchGlob)
-		if err != nil {
-			panic("Invalid glob pattern hardcoded")
-		}
-		if len(gpkgFiles) == 0 {
-			continue
-		}
-		sort.Strings(gpkgFiles)
-		gpkgFilename := gpkgFiles[0]
-		gpkgPath = path.Clean(path.Join(wd, gpkgFilename))
-		break
-	}
-
-	return gpkgPath
-}
 
 func main() {
 	var bindIP string
@@ -117,7 +80,7 @@ func main() {
 		}
 	}
 	if dataSource == "" {
-		dataSource = defaultGpkg()
+		dataSource = defaultgpkg.DefaultGpkg()
 	}
 	if dataSource == "" {
 		panic("no datasource")
