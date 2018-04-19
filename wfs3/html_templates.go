59c367dfddc8b7b18433fa4239629dc5c35ef20e
Tom Kralidis <tomkralidis@gmail.com>
2018-04-19 01:47:44 +0000

    fix html

--- wfs3/html_templates.go
+++ wfs3/html_templates.go
@@ -8,15 +8,15 @@ var tmpl_base = `<!doctype html>
 	{{ range .links }}
 	<link rel="{{ .Rel }}" type="application/json" href="{{ .Href }}"/>
 	{{ end }}
-</head>
-<body>
-	<header>
-		<h1>{{ .config.Metadata.Identification.Title }}</h1>
-		<span itemprop="description">{{ .config.Metadata.Identification.Description }}</span>
-	</header>
-	{{ .body }}
-	<footer>Powered by <a title="go-wfs" href="https://github.com/go-spatial/go-wfs">go-wfs</a></footer>
-</body>
+	</head>
+	<body>
+		<header>
+			<h1>{{ .config.Metadata.Identification.Title }}</h1>
+			<span itemprop="description">{{ .config.Metadata.Identification.Description }}</span>
+		</header>
+		{{ .body }}
+		<footer>Powered by <a title="go-wfs" href="https://github.com/go-spatial/go-wfs">go-wfs</a></footer>
+	</body>
 </html>`
 
 var tmpl_root = `
@@ -31,6 +31,6 @@ var tmpl_conformance = `
 <h2>Conformance</h2>
         <ul>
         {{ range .data.ConformsTo }}
-        <li><a href="{{ . }}">{{ . }}</a></li>
+	        <li><a href="{{ . }}">{{ . }}</a></li>
         {{ end }}
         </ul>`
